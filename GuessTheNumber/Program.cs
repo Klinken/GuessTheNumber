4b624f257fcafc0c19e3f97773ed813690289b2a
Soren <klinkwort92@gmail.com>
2021-04-29 09:30:02 +0200

    Initial commit

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+
+            // Methods
+
+            // Execution
+        }
+    }
+}
