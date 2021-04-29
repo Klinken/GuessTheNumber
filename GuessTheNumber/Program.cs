e1a98641f66f35aa7491ec92c8cd4b5e4bc4bca6
Soren <klinkwort92@gmail.com>
2021-04-29 09:45:23 +0200

    Basic structure for the game
    Added a variable to store the number that needs guessing, a method to handle the guess' and some information for the player

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -6,11 +6,30 @@ namespace GuessTheNumber
     {
         static void Main(string[] args)
         {
+
             // Variables
+            int numberToGuess = new Random().Next(1, 51);
+
 
             // Methods
+            string usersGuess()
+            {
+                Console.Write("Your guess: ");
+
+
+
+                return "Correct a mundo";
+            }
+             
 
             // Execution
+
+            Console.WriteLine("Welcome to guess the number!");
+            Console.WriteLine("The object of the game; guess the secret number, you will be told, if your to low or high.");
+
+
+
+
         }
     }
 }
